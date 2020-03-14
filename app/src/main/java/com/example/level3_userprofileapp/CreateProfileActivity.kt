b509b4ec414cd2a7eefe028c11574f574ca0757c
Yassine el Aatiaoui <yassine.el.aatiaoui@hva.nl>
2020-03-14 22:12:33 +0100

    Level 3 Learning Task 1 - User Profile App

---
+++ app/src/main/java/com/example/level3_userprofileapp/CreateProfileActivity.kt
@@ -0,0 +1,56 @@
+package com.example.level3_userprofileapp
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+const val GALLERY_REQUEST_CODE = 100
+
+class CreateProfileActivity : AppCompatActivity() {
+    private var profileImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+        initViews()
+    }
+    private fun initViews() {
+        btnGallery.setOnClickListener { onGalleryClick() }
+        btnConfirm.setOnClickListener { onConfirmClick() }
+    }
+
+
+    private fun onGalleryClick() {
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+        galleryIntent.type = "image/*"
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    private fun onConfirmClick() {
+        val profile = Profile(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etProfileDescription.text.toString(),
+            profileImageUri
+        )
+
+        val profileActivityIntent = Intent(this, ProfileActivity::class.java)
+        profileActivityIntent.putExtra(PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
+    }
+override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (resultCode == Activity.RESULT_OK) {
+        when (requestCode) {
+            GALLERY_REQUEST_CODE -> {
+                profileImageUri = data?.data
+                ivProfileImage.setImageURI(profileImageUri)
+            }
+        }
+    }
+}
+
+}
